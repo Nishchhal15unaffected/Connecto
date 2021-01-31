5dad5d1dad789d9e01ac597085cb3b0c8e13dd71
Nishchhal Prajapat <nishchhalprajapat@gmail.com>
2021-02-01 02:48:56 +0530

    setup view engine ejs

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port=8000;
 
 app.use('/',require('./routes'))
 
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
 	if(err){
 		console.log(`error in running the server: ${err}`);
